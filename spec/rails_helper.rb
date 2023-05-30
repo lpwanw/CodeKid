9ab27efd5faeed2e250ee4e364f5caa12fb34a99
taylp-2865 <le.phuong.tay@sun-asterisk.com>
2023-05-30 14:20:58 +0700

    Header UI

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,8 @@ require_relative "../config/environment"
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
-
+require "view_component/test_helpers"
+require "capybara/rspec"
 require "simplecov"
 SimpleCov.start "rails"
 
@@ -67,4 +68,12 @@ RSpec.configure do |config|
 
   # includes
   config.include FactoryBot::Syntax::Methods
+  config.include ViewComponent::TestHelpers, type: :component
+  config.include ViewComponent::SystemTestHelpers, type: :component
+  config.include Capybara::RSpecMatchers, type: :component
+  # config.include Devise::Test::ControllerHelpers, type: :component
+  #
+  # config.before(:each, type: :component) do
+  #   @request = vc_test_controller.request
+  # end
 end
